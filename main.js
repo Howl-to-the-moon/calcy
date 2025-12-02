a8e4cefa80ff38ec329ce3b5e5d590a38245596c
Howl-to-the-moon <149625575+Howl-to-the-moon@users.noreply.github.com>
2025-12-02 15:10:44 -0500

    Intial Logic Implemented
    made the calculator functions for the main symbols
    and doing an expression

--- main.js
+++ main.js
@@ -1,1 +1,59 @@
 console.log("hamana");
+
+function add (a, b) {
+    return a + b;
+};
+
+function subtract (a, b) {
+    return a - b;
+};
+
+function multiply (a, b) {
+    return a * b;
+};
+
+function divide (a, b) {
+    return a /b;
+};
+
+function operate (numOne, numTwo, operator) {
+
+switch (operator) {
+
+    case '+':
+        console.log(add(numOne, numTwo));
+    break;
+
+    case '-':
+        console.log(subtract(numOne, numTwo));
+    break;
+
+    case '*':
+        console.log(multiply(numOne, numTwo));
+    break;
+
+    case '/':
+        console.log(divide(numOne, numTwo));
+    break;
+
+    default:
+        console.log('huh');
+    break;
+}
+
+};
+
+
+
+
+
+
+
+
+// variables past here
+
+let firstNum = 0;
+let secondNum = 0;
+let operator = '';
+
+operate(20, 5, '+');
